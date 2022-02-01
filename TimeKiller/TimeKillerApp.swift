8ac025fd5b43d4850dba1b606a38a95dcd3ee7f1
Nikita Ryzhkin <timongo@bk.ru>
2022-02-01 14:02:35 +0400

    Initial Commit

---
+++ TimeKiller/TimeKillerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeKillerApp.swift
+//  TimeKiller
+//
+//  Created by Никита Рыжкин on 01.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeKillerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
